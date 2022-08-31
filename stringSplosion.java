610c7b1506c0938a214b50a4a7453162b9dd196e
Dolly Singh <68997138+dollyrajpoot07@users.noreply.github.com>
2022-09-01 01:57:58 +0530

    Update stringSplosion.java

--- stringSplosion.java
+++ stringSplosion.java
@@ -4,29 +4,25 @@
 // // stringSplosion("abc") → "aababc"
 // // stringSplosion("ab") → "aab"
 
-// import java.util.*;
+import java.util.*;
 
-// class stringSplosion {
+class stringSplosion {
     
-// public static String stringSplosion(String str) {
-//     String result = "";
-//     String residue = "";
-    
-//     // use string builder class
-    
-//     for(int i=0; i<str.length(); i++){
-//       residue = str.substring(0, i + 1);
-//       result = result + residue;
-//     }
-    
-//     return result;
-// }
-
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         String s = sc.next(); 
-//         String result = stringSplosion(s);
-//         System.out.print(result);
-//         sc.close();
-//     }
-// }
+public static String stringSplosion(String str) {
+    String result = "";
+    String residue = "";
+  
+    for(int i=0; i<str.length(); i++){
+      residue = str.substring(0, i + 1);
+      result = result + residue;
+    }
+    return result;
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(); 
+        String result = stringSplosion(s);
+        System.out.print(result);
+        sc.close();
+    }
+}
